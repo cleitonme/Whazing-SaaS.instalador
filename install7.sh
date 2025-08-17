98f2d7abf1bfbce9df124e8d7f51e9818e3c3434
cleitonme <cleitonme@gmail.com>
2025-08-16 23:08:21 -0300

    .

--- install7.sh
+++
@@ -1,6 +0,0 @@
-sudo apt install -y tmux git
-cd /root
-git clone https://github.com/cleitonme/Whazing-SaaS.instalador.git whazinginstalador
-chmod +x whazinginstalador/whazing
-tmux new-session -d -s whazing './whazing; bash'
-tmux attach -t whazing
