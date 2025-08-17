19c23f34956b899ca761a9e1681b3a9e4b7d1cba
cleitonme <cleitonme@gmail.com>
2025-08-16 22:42:52 -0300

    Create instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Atualiza o sistema
+apt update -y && apt upgrade -y
+
+# Instala dependências
+apt install -y software-properties-common git
+
+# Vai para a pasta /root
+cd /root || exit
+
+# Clona o instalador
+git clone https://github.com/cleitonme/Whazing-SaaS.instalador.git whazinginstalador
+
+# Dá permissão de execução
+chmod +x ./whazinginstalador/whazing
+
+# Executa o instalador interativo
+cd ./whazinginstalador || exit
+./whazing
